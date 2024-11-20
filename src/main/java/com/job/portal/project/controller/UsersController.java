ac1713412168e841f06e875e464b644cf217c010
kalaiyarasi <kalai021998@gmail.com>
2024-11-20 22:22:45 +0530

    Login and Logout implementation

--- src/main/java/com/job/portal/project/controller/UsersController.java
+++ src/main/java/com/job/portal/project/controller/UsersController.java
@@ -4,8 +4,13 @@ import com.job.portal.project.entity.Users;
 import com.job.portal.project.entity.UsersType;
 import com.job.portal.project.services.UsersService;
 import com.job.portal.project.services.UsersTypeService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -45,7 +50,24 @@ public class UsersController {
             return "register";
         }
         usersService.addNew(users);
-        return "dashboard";
+        return "redirect:/dashboard/";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response){
+
+        Authentication authentication = SecurityContextHolder.getContext()
+                .getAuthentication();
+
+            if (authentication != null){
+                new SecurityContextLogoutHandler().logout(request, response, authentication);
+            }
+        return "rediredt:/";
     }
 
 
